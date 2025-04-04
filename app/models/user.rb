6f9b6cfb83bb43de912aa1be54aa30b6a03a3f83
Draco <kevin.tenisson@gmail.com>
2025-04-03 19:27:43 -0700

    user validations & relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :trackable
 
+  # Relatonships
+  has_many :sessions, dependent: :destroy
+  has_many :paired_sessions, class_name: 'Session', foreign_key: 'paired_user_id'
+  has_many :transactions, dependent: :destroy
+  has_many :paired_requests, foreign_key: 'requester_id', dependent: :destroy
+  has_many :matched_requests, class_name: 'PairedRequest', foreign_key: 'matched_user_id'
+
+  # Validations
+  validates :username, presence: true, uniqueness: true
+  validates :credits, numericality: { greater_than_or_equal_to: 0 }
+
+  # Callbacks
   after_create :assign_welcome_credits
 
   private
