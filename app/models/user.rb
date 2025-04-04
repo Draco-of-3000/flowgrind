c6d19c86bdd9e19210c3bf4dfd994fc2a9cd9fd6
Draco <kevin.tenisson@gmail.com>
2025-04-03 18:40:57 -0700

    method for welcome credits

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :trackable
+
+  after_create :assign_welcome_credits
+
+  private
+
+  def assign_welcome_credits
+    update(credits: 5.0)
+  end
 end
